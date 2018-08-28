856d0aba2e3cb56628b81a4666a67de6c43c627d
peer <peerforcher@foxmail.com>
2018-08-28 09:39:57 +0000

    Finish basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- CSRF Token 方便前段的js脚本获取csrf令牌 -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>@yield('title','LaraBBS')</title>
+
+    <!-- Styles :asset('css/app.css') 使用当前请求的协议（ HTTP 或 HTTPS ）为资源文件生成一个 URL-->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <!-- route_class() 自定义的辅助方法 -->
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>  
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
