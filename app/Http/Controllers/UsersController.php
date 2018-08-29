c4ed61366d900694acf6482fa7b1a93c0d0d25e9
peer <peerforcher@foxmail.com>
2018-08-29 10:09:02 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
