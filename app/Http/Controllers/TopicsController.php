12801b041626c17114f650627b00516e2145fd26
peer <peerforcher@foxmail.com>
2018-08-30 18:02:16 +0000

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Handlers\ImageUploadHandler;
 use Auth;
 
 class TopicsController extends Controller
@@ -63,4 +64,25 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+    public function uploadImage(Request $request,ImageUploadHandler $uploader){
+        //初始化返回数据，默认是失败的
+        $data = [
+            'success' => false,
+            'msg' => '上传失败！',
+            'file_path' => ''
+        ];
+        //判断是否有文件上传，并赋值给$file
+        if($file = $request->upload_file){
+            //保存图片到本地
+            $result = $uploader->save($request->upload_file,'topics',Auth::id(),1024);
+            //图片保存成功的话
+            if($result){
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功';
+                $data['success'] = true;
+            }
+        }
+        return $data;
+    }
 }
