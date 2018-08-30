3d2eb24692074aaff23dd53976a0304eb4a5f55b
peer <peerforcher@foxmail.com>
2018-08-30 14:05:02 +0000

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,11 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
-        //读取分类id关联的话题，并按每20条分页
-        $topics = Topic::where('category_id',$category->id)->with('category','user')->paginate(20);
+    public function show(Category $category,Request $request,Topic $topic){
+        //读取分类id关联的话题，并按每30条分页
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id',$category->id)
+                        ->paginate(30);
         //传参变量话题和分类到模板中
         return view('topics.index',compact('topics','category'));
     }
