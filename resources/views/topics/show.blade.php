025d9dcaf59be550011b46644995f6a73a70e3c1
peer <peerforcher@foxmail.com>
2018-09-03 10:01:17 +0000

    Author can delete topic

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -43,16 +43,21 @@
                     {!! $topic->body !!}
                 </div>
 
+            @can('update',$topic)
                 <div class="operate">
                     <hr>
-                    <a href="{{ route('topics.edit',$topic->id) }}" class="btn btn-default btn-xs">
+                    <a href="{{ route('topics.edit',$topic->id) }}" class="btn btn-default btn-xs pull-left" role="button">
                         <i class="glyphicon glyphicon-edit"></i> 编辑
                     </a>
-                    <a href="#" class="btn btn-default btn-xs">
-                        <i class="glyphicon glyphicon-trash"></i> 删除
-                    </a>
+                    <form action="{{ route('topics.destroy',$topic->id) }}" method="post">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button class="btn btn-default btn-xs pull-left" style="margin-left: 6px" type="submit">
+                            <i class="glyphicon glyphicon-trash"></i> 删除
+                        </button>
+                    </form>
                 </div>
-
+            @endcan
             </div>
         </div>
     </div>
