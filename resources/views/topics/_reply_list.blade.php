ad2f7ef3b98db535a5ef1f7c45dfb5b10aa05812
peer <peerforcher@foxmail.com>
2018-09-04 14:08:49 +0000

    User can delete reply

--- resources/views/topics/_reply_list.blade.php
+++ resources/views/topics/_reply_list.blade.php
@@ -18,11 +18,17 @@
                     </span>
 
                     {{-- 回复删除按钮 --}}
-                    <span class="meta pull-right">
-                        <a title="删除回复">
-                            <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                        </a>
-                    </span>
+                    @can('destroy',$reply)
+                        <span class="meta pull-right">
+                            <form action="{{ route('replies.destroy',$reply->id) }}" method="post">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button class="btn btn-default btn-xs pull-left">
+                                    <i class="glyphicon glyphicon-trash delete" aria-hidden="true"></i>
+                                </button>
+                            </form>
+                        </span>
+                    @endcan
                 </div>
                 <div class="reply-content">
                     {!! $reply->content !!}
