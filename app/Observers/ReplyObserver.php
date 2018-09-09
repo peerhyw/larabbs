4ba0efe98d6f94137e1b3746b9138d77f68d108f
peer <peerforcher@foxmail.com>
2018-09-09 15:29:23 +0000

    Add last reply user id

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,12 +4,14 @@ namespace App\Observers;
 
 use App\Models\Reply;
 use App\Notifications\TopicReplied;
+use Auth;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
+
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content,'user_topic_body');
@@ -22,6 +24,11 @@ class ReplyObserver
 
     public function created(Reply $reply){
         $topic = $reply->topic;
+
+        $topic->last_reply_user_id = Auth::user()->id;
+
+        $topic->save();
+
         $topic->increment('reply_count',1);
 
         //通知作者话题被回复了
@@ -29,6 +36,20 @@ class ReplyObserver
     }
 
     public function deleted(Reply $reply){
-        $reply->topic->decrement('reply_count',1);
+        $topic = $reply->topic;
+
+        if($topic->reply_count === 1){
+            $topic->last_reply_user_id = 0;
+        }
+        else{
+            $result = Reply::select('user_id')->where('topic_id',$topic->id)->orderBy('created_at','desc')->first();
+
+            $topic->last_reply_user_id = $result['user_id'];
+        }
+
+        $topic->save();
+
+        $topic->decrement('reply_count',1);
+
     }
 }
