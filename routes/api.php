9890ddb92d94d13dfbe999062cbf0b96d3a42b5c
peer <peerforcher@foxmail.com>
2018-09-10 18:00:59 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,15 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',[
     'namespace' => 'App\Http\Controllers\Api'
 ],function ($api){
-    //短信验证码
-    $api->post('verificationCodes','VerificationCodesController@store')->name('api.verificationCodes.store');
-    //用户注册
-    $api->post('users','UsersController@store')->name('api.users.store');
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'), //1分钟1次api请求
+    ],function ($api){
+        //短信验证码
+        $api->post('verificationCodes','VerificationCodesController@store')->name('api.verificationCodes.store');
+        //用户注册
+        $api->post('users','UsersController@store')->name('api.users.store');
+    });
 });
