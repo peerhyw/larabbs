9338ccdcd91c2d5e39a41795e6e9cc3c89e061df
peer <peerforcher@foxmail.com>
2018-09-13 17:53:13 +0000

    topics show

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -60,4 +60,8 @@ class TopicsController extends Controller
         $topics = $user->topics()->recent()->paginate(20);
         return $this->response->paginator($topics,new TopicTransformer);
     }
+
+    public function show(Topic $topic){
+        return $this->response->item($topic,new TopicTransformer());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,24 +27,34 @@ $api->version('v1',[
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'), //1分钟1次api请求
     ],function ($api){
+
         //短信验证码
         $api->post('verificationCodes','VerificationCodesController@store')->name('api.verificationCodes.store');
+
         //用户注册
         $api->post('users','UsersController@store')->name('api.users.store');
+
         //图片验证码
         $api->post('captchas','CaptchasController@store')->name('api.captchas.store');
+
         //第三方登录
         $api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')->name('api.socials.authorizations.store');
+
         //登录
         $api->post('authorizations','AuthorizationsController@store')->name('api.authorizations.store');
+
         //刷新token
         $api->put('authorizations/current','AuthorizationsController@update')->name('api.authorizations.update');
+
         //删除token
         $api->delete('authorizations/current','AuthorizationsController@destroy')->name('api.authorizations.destroy');
+
         //游客可以访问的接口
         $api->get('categories','CategoriesController@index')->name('api.categories.index');
         $api->get('topics','TopicsController@index')->name('api.topics.index');
+        $api->get('topics/{topic}','TopicsController@show')->name('api.topics.show');
         $api->get('users/{user}/topics','TopicsController@userIndex')->name('api.users.topics.index');
+
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function ($api){
             //当前登录用户信息
