95d8fe187a3647d8c07c829da04388ca1d215873
peer <peerforcher@foxmail.com>
2018-09-13 16:43:24 +0000

    topic update api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -16,4 +16,12 @@ class TopicsController extends Controller
 
         return $this->response->item($topic,new TopicTransformer())->setStatusCode(201);
     }
+
+    public function update(TopicRequest $request,Topic $topic){
+        //重要一句代码
+        $this->authorize('update',$topic);
+
+        $topic->update($request->all());
+        return $this->response->item($topic,new TopicTransformer());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',[
     'namespace' => 'App\Http\Controllers\Api',
     //DataArraySerializer 和 ArraySerializer 两种格式可switch
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array','bindings']
 ],function ($api){
 
     $api->group([
@@ -53,6 +53,8 @@ $api->version('v1',[
             $api->post('images','ImagesController@store')->name('api.images.store');
             //发布话题
             $api->post('topics','TopicsController@store')->name('api.topics.store');
+            //修改话题
+            $api->patch('topics/{topic}','TopicsController@update')->name('api.topics.update');
         });
     });
 });
