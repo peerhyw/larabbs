3d04b66d23a87c4be7f3374e3cb3c4ae143c06e4
peer <peerforcher@foxmail.com>
2018-09-14 07:48:20 +0000

    replies index

--- app/Transformers/ReplyTransformer.php
+++ app/Transformers/ReplyTransformer.php
@@ -10,6 +10,10 @@ use League\Fractal\TransformerAbstract;
  */
 class ReplyTransformer extends TransformerAbstract
 {
+    //逗号 —— 是当前资源所关联的资源，如 include=topic,user；
+    //点 —— 当前资源所关联的资源，及其所关联的资源，相当于下一级资源，如 include=topic.user
+    protected $availableIncludes = ['user','topic'];
+
     public function transform(Reply $reply){
         return [
             'id' => $reply->id,
@@ -20,4 +24,13 @@ class ReplyTransformer extends TransformerAbstract
             'updated_at' => $reply->updated_at->toDateTimeString(),
         ];
     }
+
+    public function includeUser(Reply $reply){
+        return $this->item($reply->user,new UserTransformer());
+    }
+
+    public function includeTopic(Reply $reply){
+        return $this->item($reply->topic,new TopicTransformer());
+    }
+
 }
