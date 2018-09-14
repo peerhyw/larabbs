587c7dfe7ac4baed253511d5c4e7fbb03d1709ac
peer <peerforcher@foxmail.com>
2018-09-14 18:17:33 +0000

    actived users

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -67,4 +67,8 @@ class UsersController extends Controller
         $user->update($attributes);
         return $this->response->item($user,new UserTransformer());
     }
+
+    public function activedIndex(User $user){
+        return $this->response->collection($user->getActiveUsers(),new UserTransformer());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -64,6 +64,8 @@ $api->version('v1',[
         $api->get('users/{user}/replies','RepliesController@userIndex')->name('api.users.replies.index');
         //资源推荐
         $api->get('links','LinksController@index')->name('api.links.index');
+        //活跃用户
+        $api->get('actived/users','UsersController@activedIndex')->name('api.actived.users.index');
 
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function ($api){
